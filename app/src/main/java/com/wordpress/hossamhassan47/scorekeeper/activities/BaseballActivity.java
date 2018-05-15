198e20f5ef0a6e9cc72f02b12866c80746837a4b
HossamHassan47 <hossam.hassan@outlook.com>
2018-05-15 11:05:19 +0200

    baseball finalize

--- app/src/main/java/com/wordpress/hossamhassan47/scorekeeper/activities/BaseballActivity.java
+++ app/src/main/java/com/wordpress/hossamhassan47/scorekeeper/activities/BaseballActivity.java
@@ -5,11 +5,33 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.wordpress.hossamhassan47.scorekeeper.R;
+import com.wordpress.hossamhassan47.scorekeeper.fragments.AddNewMatchFragment;
 
 public class BaseballActivity extends AppCompatActivity {
+
+    String teamAName;
+    String teamBName;
+
+    int teamAScoreRuns = 0;
+    int teamAScoreOuts = 0;
+
+    int teamBScoreRuns = 0;
+    int teamBScoreOuts = 0;
+
+    TextView txtTeamAScoreRuns;
+    TextView txtTeamBScoreRuns;
+    TextView txtTeamAScoreOuts;
+    TextView txtTeamBScoreOuts;
+
+    TextView txtTeamAName;
+    TextView txtTeamBName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +40,88 @@ public class BaseballActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        // Set Team A Name
+        teamAName = getIntent().getExtras().getString("teamA");
+        txtTeamAName = findViewById(R.id.text_view_team_a_name);
+        txtTeamAName.setText(teamAName);
+
+        // Set Team B Name
+        teamBName = getIntent().getExtras().getString("teamB");
+        txtTeamBName = findViewById(R.id.text_view_team_b_name);
+        txtTeamBName.setText(teamBName);
+
+        // Team A Score Rums
+        txtTeamAScoreRuns = findViewById(R.id.text_view_team_a_score_runs);
+        txtTeamAScoreRuns.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                teamAScoreRuns++;
+                txtTeamAScoreRuns.setText(teamAScoreRuns + "");
+            }
+        });
+
+        // Team B Score Rums
+        txtTeamBScoreRuns = findViewById(R.id.text_view_team_b_score_runs);
+        txtTeamBScoreRuns.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                teamBScoreRuns++;
+                txtTeamBScoreRuns.setText(teamBScoreRuns + "");
+            }
+        });
+
+        // Team A Score Outs
+        txtTeamAScoreOuts = findViewById(R.id.text_view_team_a_score_outs);
+        txtTeamAScoreOuts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                teamAScoreOuts++;
+                txtTeamAScoreOuts.setText(teamAScoreOuts + "");
+            }
+        });
+
+        // Team B Score Outs
+        txtTeamBScoreOuts = findViewById(R.id.text_view_team_b_score_outs);
+        txtTeamBScoreOuts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                teamBScoreOuts++;
+                txtTeamBScoreOuts.setText(teamBScoreOuts + "");
             }
         });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_reset) {
+            Reset();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void Reset() {
+        teamAScoreOuts = 0;
+        teamAScoreRuns = 0;
+        teamBScoreOuts = 0;
+        teamBScoreRuns = 0;
+
+        txtTeamAScoreRuns.setText("0");
+        txtTeamAScoreOuts.setText("0");
+        txtTeamBScoreRuns.setText("0");
+        txtTeamBScoreOuts.setText("0");
+    }
 }
